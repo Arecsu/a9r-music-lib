0af5f06372e22a181fae880fdcd9c08b521e9019
Arecsu <alejandro9r@gmail.com>
2022-04-02 15:53:36 -0300

    better no-media

--- no-media.sh
+++ no-media.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-find $1 -mindepth 1 \( ! -regex '.*/\..*' \) -type d -exec sh -c 'for d; do touch "$d/.NOMEDIA"; done' _ {} +
+no_media() {
+   echo $1
+   touch "$1/.NOMEDIA"
+}
+
+
+export -f no_media
+
+echo "Working..."
+
+# excludes hidden directories and shows not empty dirs
+# but it will include directores with other directories inside
+# >>>>>>
+# find $1 -mindepth 1 \( ! -regex '.*/\..*' \) -not -empty -type d | parallel --jobs 2 no_media {}
+
+# excludes hidden directories and shows dirs with at least one file
+find $1 -mindepth 2 -not -path '*/.*' -type f -print0 | xargs -0 -n 1 dirname | sort -u | parallel --jobs 2 no_media {}
+
+# folders that contain jpg, png or opus
+# then sed will leave just the directory path without the file
+# sort will sort it
+# uniq will remove duplicates
+# >>>>>>>>
+# find . -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.opus \) | sed -r 's|/[^/]+$||' |sort |uniq
+
+
 
